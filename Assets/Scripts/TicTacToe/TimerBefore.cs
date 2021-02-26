115bb2104e084639ae417b91bcb1ad174f7eb091
Madeline <bertrandemilien@gmail.com>
2021-02-26 20:52:48 +0100

    Oopsie Woopsie

--- Assets/Scripts/TicTacToe/TimerBefore.cs
+++ Assets/Scripts/TicTacToe/TimerBefore.cs
@@ -5,17 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class TimerBefore : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public GameObject error1;
+    public GameObject error2;
 
+
+    private void Start()
+    {
+        StartCoroutine(Timer());    
+    }
 
     private IEnumerator Timer()
     {
-        yield return new WaitForSeconds(7f);
+        yield return new WaitForSeconds(5f);
+
+        error1.SetActive(true);
+        FindObjectOfType<AudioManager>().Play("Error");
 
         yield return new WaitForSeconds(2f);
+
+        error2.SetActive(true);
+        FindObjectOfType<AudioManager>().Play("Error");
+
+        yield return new WaitForSeconds(0.25f);
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
