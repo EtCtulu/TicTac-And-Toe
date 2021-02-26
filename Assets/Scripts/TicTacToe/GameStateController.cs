b1a39b2f9dabc15639a6310de3576a644e366e45
Madeline <bertrandemilien@gmail.com>
2021-02-26 19:42:45 +0100

    Ajout scene tic tac toe + début du timer

---
+++ Assets/Scripts/TicTacToe/GameStateController.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameStateController : MonoBehaviour
+{
+    [Header("TitleBar References")]
+    public Image playerXIcon;                                       
+    public Image playerOIcon;                                      
+    public InputField player1InputField;                             
+    public InputField player2InputField;                            
+    public Text winnerText;                                          
+
+    [Header("Misc References")]
+    public GameObject endGameState;                               
+
+    [Header("Asset References")]
+    public Sprite tilePlayerO;                                  
+    public Sprite tilePlayerX;                                      
+    public Sprite tileEmpty;                                   
+    public Text[] tileList;                                     
+
+    [Header("GameState Settings")]
+    public Color inactivePlayerColor;                        
+    public Color activePlayerColor;                               
+    public string whoPlaysFirst;                                   
+
+    [Header("Private Variables")]
+    private string playerTurn;                                      
+    private string player1Name;                                     
+    private string player2Name;                                     
+    private int moveCount;                                          
+
+
+
+    /// <summary>
+    /// Start is called on the first active frame
+    /// </summary>
+    private void Start()
+    {
+       
+        playerTurn = whoPlaysFirst;
+        if (playerTurn == "X") playerOIcon.color = inactivePlayerColor;
+        else playerXIcon.color = inactivePlayerColor;
+
+       
+        player1InputField.onValueChanged.AddListener(delegate { OnPlayer1NameChanged(); });
+        player2InputField.onValueChanged.AddListener(delegate { OnPlayer2NameChanged(); });
+
+     
+        player1Name = player1InputField.text;
+        player2Name = player2InputField.text;
+    }
+
+    /// <summary>
+    /// Called at the end of every turn to check for win conditions
+    /// Hardcoded all possible win conditions (8)
+    /// We just take position of tiles and check the neighbours (within a row)
+    /// 
+    /// Tiles are numbered 0..8 from left to right, row by row, example:
+    /// [0][1][2]
+    /// [3][4][5]
+    /// [6][7][8]
+    /// </summary>
+    public void EndTurn()
+    {
+        moveCount++;
+        if (tileList[0].text == playerTurn && tileList[1].text == playerTurn && tileList[2].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[3].text == playerTurn && tileList[4].text == playerTurn && tileList[5].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[6].text == playerTurn && tileList[7].text == playerTurn && tileList[8].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[0].text == playerTurn && tileList[3].text == playerTurn && tileList[6].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[1].text == playerTurn && tileList[4].text == playerTurn && tileList[7].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[2].text == playerTurn && tileList[5].text == playerTurn && tileList[8].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[0].text == playerTurn && tileList[4].text == playerTurn && tileList[8].text == playerTurn) GameOver(playerTurn);
+        else if (tileList[2].text == playerTurn && tileList[4].text == playerTurn && tileList[6].text == playerTurn) GameOver(playerTurn);
+        else if (moveCount >= 9) GameOver("D");
+        else
+            ChangeTurn();
+    }
+
+    /// <summary>
+    /// Changes the internal tracker for whos turn it is
+    /// </summary>
+    public void ChangeTurn()
+    {
+      
+       
+        playerTurn = (playerTurn == "X") ? "O" : "X";
+        if (playerTurn == "X")
+        {
+            playerXIcon.color = activePlayerColor;
+            playerOIcon.color = inactivePlayerColor;
+        }
+        else
+        {
+            playerXIcon.color = inactivePlayerColor;
+            playerOIcon.color = activePlayerColor;
+        }
+    }
+
+    /// <summary>
+    /// Called when the game has found a win condition or draw
+    /// </summary>
+    /// <param name="winningPlayer">X O D</param>
+    private void GameOver(string winningPlayer)
+    {
+        switch (winningPlayer)
+        {
+            case "D":
+                winnerText.text = "DRAW";
+                break;
+            case "X":
+                winnerText.text = player1Name;
+                break;
+            case "O":
+                winnerText.text = player2Name;
+                break;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        endGameState.SetActive(true);
+        ToggleButtonState(false);
+    }
+
+    /// <summary>
+    /// Restarts the game state
+    /// </summary>
+    public void RestartGame()
+    {
+      
+        moveCount = 0;
+        playerTurn = whoPlaysFirst;
+        ToggleButtonState(true);
+        endGameState.SetActive(false);
+
+   
+        for (int i = 0; i < tileList.Length; i++)
+        {
+            tileList[i].GetComponentInParent<TileController>().ResetTile();
+        }
+    }
+
+    /// <summary>
+    /// Enables or disables all the buttons
+    /// </summary>
+    private void ToggleButtonState(bool state)
+    {
+        for (int i = 0; i < tileList.Length; i++)
+        {
+            tileList[i].GetComponentInParent<Button>().interactable = state;
+        }
+    }
+
+    /// <summary>
+    /// Returns the current players turn (X / O)
+    /// </summary>
+    public string GetPlayersTurn()
+    {
+        return playerTurn;
+    }
+
+    /// <summary>
+    /// Retruns the display sprite (X / 0)
+    /// </summary>
+    public Sprite GetPlayerSprite()
+    {
+        if (playerTurn == "X") return tilePlayerX;
+        else return tilePlayerO;
+    }
+
+    /// <summary>
+    /// Callback for when the P1_textfield is updated. We just update the string for Player1
+    /// </summary>
+    public void OnPlayer1NameChanged()
+    {
+        player1Name = player1InputField.text;
+    }
+
+    /// <summary>
+    /// Callback for when the P2_textfield is updated. We just update the string for Player2
+    /// </summary>
+    public void OnPlayer2NameChanged()
+    {
+        player2Name = player2InputField.text;
+    }
+}
---
+++ Assets/Scripts/TicTacToe/TileController.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileController : MonoBehaviour
+{
+    [Header("Component References")]
+    public GameStateController gameController;                   
+    public Button interactiveButton;                            
+    public Text internalText;                                      
+
+
+
+    /// <summary>
+    /// Called everytime we press the button, we update the state of this tile.
+    /// The internal tracking for whos position (the text component) and disable the button
+    /// </summary>
+    public void UpdateTile()
+    {
+        internalText.text = gameController.GetPlayersTurn();
+        interactiveButton.image.sprite = gameController.GetPlayerSprite();
+        interactiveButton.interactable = false;
+        gameController.EndTurn();
+    }
+
+    /// <summary>
+    /// Resets the tile properties
+    /// - text component
+    /// - buttton image
+    /// </summary>
+    public void ResetTile()
+    {
+        internalText.text = "";
+        interactiveButton.image.sprite = gameController.tileEmpty;
+    }
+}
