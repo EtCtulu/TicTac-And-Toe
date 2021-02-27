d39217e4d204ae329b4871b6fec3d87a4a2a8e87
Samuel <ARSAC>
2021-02-27 12:40:47 +0100

    Sélection du crush blocking done + transitions vers chaque scène correspondante done

---
+++ Assets/Scripts/Others/ChoiceCrush.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChoiceCrush : MonoBehaviour
+{
+    public void ChooseKaren()
+    {
+        SceneManager.LoadScene("Bar");
+    }
+
+    public void ChoosePipper()
+    {
+        SceneManager.LoadScene("Parc");
+    }
+
+    public void ChooseArnold()
+    {
+        SceneManager.LoadScene("Plage");
+    }
+
+}
